43d1a22970c3640fb712f1561be96a32e97e5ca2
GiovaniWHB <giovaniwhb@gmail.com>
2025-01-31 21:55:08 -0300

    feat: add `src` folder
    Além da pasta src ter sido adicionada, foi alterada a maneira de carregar a font do projeto, agora
    sendo adicionada pelo next/font/google no layout ao invés do css global.

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,16 +1,21 @@
-import { Metadata } from 'next';
-import '../styles/globals.css';
+import type { Metadata } from 'next';
+import { Montserrat } from 'next/font/google';
+import '@/src/styles/globals.css';
 
 export const metadata: Metadata = {
     title: 'RS Solutions Challenge',
     description: 'Desafio da empresa RS Solutions estilo Hackathon',
 };
 
+const montserrat = Montserrat({
+    subsets: ['latin'],
+});
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="pt-BR">
+        <html lang="pt-BR" className={montserrat.className}>
             <body>
-                <main>{children}</main>
+                <div>{children}</div>
             </body>
         </html>
     );
