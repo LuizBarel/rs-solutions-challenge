c64698bc02e1afc5b0700b2f8c6c42900af250fb
Giovani-Bianchi <giovaniwhb@gmail.com>
2025-02-08 20:58:42 -0300

    fix: fixing the `main` border that disappear after scrolling
    Foi adicionado uma borda ao `header` após o scroll da página, visando substituir a borda do main que
    desaparece após o scroll por conta do `header` fixo. Foi corrigido também a identação do código no
    arquivo do `chart`.

--- front-end/src/app/dashboard/layout.tsx
+++ front-end/src/app/dashboard/layout.tsx
@@ -1,17 +1,40 @@
 // Definindo o componente para ser renderizado no lado do cliente (client side)
 'use client';
 
+import { useEffect, useState } from 'react';
+import { cn } from '@/lib/utils';
+
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { AppSidebar } from '@/components/layouts/app-sidebar';
 
 import { HiOutlineUserCircle } from 'react-icons/hi2';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+    // Estado para controlar o scroll da página
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 0);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <SidebarProvider>
             <AppSidebar />
 
-            <header className="w-full h-[100px] fixed flex md:justify-end justify-between bg-white p-8">
+            <header
+                className={cn(
+                    'w-full h-[100px] fixed flex md:justify-end justify-between bg-white p-8',
+                    isScrolled ? 'border-b border-b-gray-600' : '',
+                )}
+            >
                 <SidebarTrigger className="flex md:hidden" />
 
                 <div className="flex items-center gap-[2px] select-none">
