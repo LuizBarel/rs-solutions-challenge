1568a62d249f9ecd980f8018ea77ca1c4efd4b30
Luiz Ricardo Barel <luizricardo.c.barel@gmail.com>
2025-02-13 15:44:23 -0300

    feat: create `seru-api` service, module and functions

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { SeruApiModule } from './seru-api/seru-api.module';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-    imports: [],
+    imports: [SeruApiModule, HttpModule, ScheduleModule.forRoot()],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
