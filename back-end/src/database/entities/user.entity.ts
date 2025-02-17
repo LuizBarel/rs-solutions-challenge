5ee088b166dc9e899fc446baed23befdfbbe70d7
Luiz Ricardo Barel <luizricardo.c.barel@gmail.com>
2025-02-16 22:00:39 -0300

    feat: create user resource and configurate typeormmodule on `app.module.ts` file

---
+++ back-end/src/database/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updatedAt: Date;
+
+    @Column({ nullable: true })
+    token: string;
+}
